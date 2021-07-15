13bb36ba22450894cbc59c5276fdba6aebd0b764
Amar-and-Stuff <64824478+Amar-and-Stuff@users.noreply.github.com>
2021-07-15 11:44:40 +0530

    stopped permeture results and limited input
    Before we made arrangement 100 times for all N. It works for small numbers not like 2, 3, 4. Not for 10, 12 etc. We ditched number of times method and implemented precision method. Which waits until every point is place at correct place with precision of 1e-10. For this to work we need to limit our input to 20. Yeah, QUALITY over QUANTITY.

--- sphere_problem.py
+++ sphere_problem.py
@@ -1,3 +1,13 @@
+'''what is this program?
+    Assume we have N points on a sphere. points are always pushing each other and they cannot leave surface of the sphere. 
+    After some play we will see the points in a state of equilibrium. If N > 3 points form a 3d solid. 
+    Interestingly we can have a solid with declaring N.(ofcourse they are specific type of solids)
+    I wanted to study those solids. so we have this program which gives scatter plot of points of those solids. 
+    '''
+
+
+
+
 import math
 import numpy as np
 import random
@@ -5,17 +15,20 @@ from operator import add, sub
 import matplotlib.pyplot as plt
    
    
-def normalize(lst):#takes a list of coordinates. Returns a normalized version of it. 
+def magnitude(lst):#takes a list of coordinates. Returns magnitude value of that position vector. 
     sqr_sum=0
 
     for i in lst:
         sqr_sum += i*i
 
-    magnitude = math.sqrt(sqr_sum)
+    magnitude_value = math.sqrt(sqr_sum)
+    return magnitude_value
+
+def normalize(lst):#takes a list of coordinates. Returns a normalized version of it. 
     nrm_lst = []
 
     for i in lst:
-        nrm_lst.append(i/magnitude)
+        nrm_lst.append(i/magnitude(lst))
 
     return nrm_lst
 
@@ -35,7 +48,7 @@ def angle_bw(v1,v2): #takes two lists of coordinates of point. Returns angle in 
 
 
 
-def points_on_unit_sphere(N,condition=100):
+def points_on_unit_sphere(N):
 
 
     #generating N points from specified range to get unique points
@@ -53,30 +66,34 @@ def points_on_unit_sphere(N,condition=100):
 
     #force_vector to find direction of force form all other points
     force_vector = [0,0,0]
-
-
+    condition = True
     while condition:
         #in this loop:
         #for a point we find all vectors looking at the point. 
         #Add them and normalize resultant vector. 
         #Make that resultant unit vector as positional vector of the point.
+        max_jump_distance = 0
         for i in range(N):
             for j in range(N):
                 if rectangular[i] == rectangular[j]:
                     continue
                 force_vector = list(map(add,force_vector,normalize(list(map(sub,rectangular[i],rectangular[j])))))
             force_vector = normalize(force_vector)
+            #here we make sure if points are stabalized or not
+            jump_distance = magnitude(list(map(sub,force_vector,rectangular[i])))
+            if jump_distance > max_jump_distance:
+                max_jump_distance = jump_distance
             rectangular[i] = force_vector
             force_vector = [0,0,0]
-        condition -= 1
-
+        if max_jump_distance < 0.0000000001:
+            condition = False
     #returning finalized rectangular coordinates to plot them.
     return rectangular
 
 
-N = int(input("enter number of points: "))
+N = int(input("Select number of points from 2 to 20: "))
 
-if N >= 2:#two points are minimum to see action.
+if N >= 2 and N <= 20:#two points are minimum to see action.
     #storing finalized rectangular coordinates in final_points.
     final_points = points_on_unit_sphere(N)
 
@@ -88,4 +105,4 @@ if N >= 2:#two points are minimum to see action.
     ax.scatter(x,y,z)
     plt.show()
 else:#mad or wht
-    print("mad or wht")
+    print("Number of points should be an integer in range of [2,20]")
